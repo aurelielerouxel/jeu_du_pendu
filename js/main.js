97fb4c566ed88b83dbff7a04f753096af7097e75
Aurelie Lerouxel <malkiniou@gmail.com>
2020-07-12 16:59:17 +0200

    add word list in jeu_du_pendu

--- js/main.js
+++ js/main.js
@@ -1,2 +1,8 @@
 //Afficher une alerte de bienvenue
 alert("Bienvenue sur le jeu du pendu spécial jeux vidéo");
+
+//Créer une liste de mots
+const wordList = ["minecraft", "tetris", "grand theft auto", "world of warcraft", " super mario bros",
+                "the legend of zelda", "mario kart", "pokemon", "red dead redemption", "diablo", "fallout"];
+console.log(wordList);
+
